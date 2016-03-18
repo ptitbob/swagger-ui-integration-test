1d3a8755b1a1cd1529d1751674974763848823ad
ptitbob <github@shipstone.org>
2016-03-18 01:18:27 +0100

    check imports

--- parameterized/src/main/java/org/shipstone/swagger/demo/configuration/ParameterizedApplicationConfiguration.java
+++ parameterized/src/main/java/org/shipstone/swagger/demo/configuration/ParameterizedApplicationConfiguration.java
@@ -1,18 +1,15 @@
 package org.shipstone.swagger.demo.configuration;
 
-import org.ocpsoft.rewrite.annotation.RewriteConfiguration;
 import org.shipstone.swagger.demo.ws.rs.RestApplication;
-import org.shipstone.swagger.integration.AbstractSwaggerURLRewriter;
-import org.shipstone.swagger.integration.SwaggerUIConfiguration;
+import org.shipstone.swagger.integration.annotation.SwaggerUIConfiguration;
 
 /**
  * @author francois
  */
-@RewriteConfiguration
 @SwaggerUIConfiguration(
     restApplicationClass = RestApplication.class
     , apiDocPath = "documentation"
     , apiDocIndex = "rest-index/index.html"
 )
-public class ParameterizedApplicationConfiguration extends AbstractSwaggerURLRewriter {
+public class ParameterizedApplicationConfiguration {
 }
